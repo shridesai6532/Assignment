9d687b5e3a1c584102f4d8c57312eabd09d2377b
HP <HP@LAPTOP-AOPGMUS1>
2023-12-05 18:43:53 +0530

    Object creation updates

---
+++ src/oops/ChatPractice.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class ChatPractice {
+	
+	public static void main(String[] args) {
+		
+		// how to create object of a class
+		
+		Human h1 = new Human();
+		h1.name = "Shri";
+		h1.age = 28;
+		
+		Human h2 = new Human();
+		h2.name = "Rushi";
+		h2.age = 24;
+		
+		h1.say ("Hello");
+		h2.say ("Hi..I am good");
+				
+	}
+
+}
---
+++ src/oops/MultipleClasses.java
@@ -0,0 +1,171 @@
+package oops;
+
+public class MultipleClasses {
+	
+
+}
+
+class Human {
+	String name;
+	String gender;
+	int age;
+	
+	void say(String content) {
+		System.out.println(name+"  -  "+ content);
+	}
+}
+
+class Animal {
+	String name;
+	String color;
+	int age;
+	String food;
+	
+	void talk() {
+		
+	}
+}
+
+class Car {
+	String brandname;
+	int model;
+	String tyretype;
+	int enginecc = 1200;
+	
+	void run() {
+		
+	}	
+}
+
+class Smartphone {
+	String name;
+	String brand;
+	double price;
+	String battery;
+	
+	void entertainment() {
+		
+	}
+}
+
+class Movie {
+	String name;
+	String release;
+	String language;
+	int ticketprice;
+	
+	void fun() {
+		
+	}
+}
+
+class Laptop {
+	String name;
+	double price;
+	String generation;
+	String screen;
+	
+	void display() {
+		
+	}
+}
+
+class State {
+	String name;
+	double population;
+	String language;
+	int district;
+	
+	void capacity() {
+		
+	}
+}
+
+class shooes {
+	String name;
+	int price;
+	String type;
+	String material;
+	
+	void sports() {
+		
+	}
+}
+
+class Watch {
+	String name;
+	String model;
+	String type;
+	int price;
+	int weight;
+	
+	void time() {
+		
+	}
+}
+
+class house {
+	String housename;
+	int rooms;
+	String type;
+	double price;
+	
+	void home() {
+		
+	}
+}
+
+class River {
+	String name;
+	String frompoint;
+	String lenght;
+	
+	void water () {
+		
+	}	
+}
+
+class college {
+	String name;
+	int stu_capacity;
+	String medium;
+	String location;
+	
+	
+	void education() {
+		
+	}
+}
+
+class books {
+	String name;
+	int price;
+	String Auther;
+	int pages;
+	
+	void read() {
+		
+	}
+}
+
+class occupation {
+	String occup_name;
+	String domain;
+	int income;
+	int investment;
+	
+	void work() {
+		
+	}
+}
+
+class Music {
+	String Singer;
+	String song;
+	int likes;
+	String release;
+	
+	void song() {
+		
+	}
+}
