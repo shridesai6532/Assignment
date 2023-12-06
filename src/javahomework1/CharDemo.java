372f829a3b0704b5d7d4b184c7fb562401ce88c8
HP <HP@LAPTOP-AOPGMUS1>
2023-12-06 14:02:33 +0530

    Name in native language

---
+++ src/javahomework1/CharDemo.java
@@ -0,0 +1,41 @@
+package javahomework1;
+
+import org.testng.annotations.Test;
+
+public class CharDemo {
+
+@Test
+	public void main() {
+		
+		
+//		char c = 'A';
+//		System.out.println(c);
+//		
+		 // converting from int to char
+		
+//		char c1 = 'J';
+//		int jk = (char)c1;   // here we did type casting  (char to int )
+//		System.out.println(jk);
+//		
+		// converting from int to char
+//		
+//		int i = 83;
+//		char ck = (char)i;
+//		System.out.println(ck);
+//		
+		
+		// Unicode char type (size is 2 bytes)
+		
+		char c1 = '\u0936';
+		char c2 = '\u093F';
+		char c3 = '\u0935';
+		
+		System.out.println(c1+""+c2+""+c3);
+		
+		
+		
+	
+
+	}
+
+}
