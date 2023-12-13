611f830656316e09546a78f07a97baf99b31f760
HP <HP@LAPTOP-AOPGMUS1>
2023-12-13 10:00:09 +0530

    class assignment

---
+++ src/oops/ClassAssignment.java
@@ -0,0 +1,47 @@
+package oops;
+
+public class ClassAssignment {
+
+
+}
+
+class HumanA{
+	String name;
+	int age;
+	String color;
+	
+	void say() {
+		System.out.println();
+	}
+}
+
+class LaptopA {
+	double Prize;
+	String color;
+	String ram;
+	
+	void work() {
+		System.out.println();
+	}
+}
+
+class Sportman {
+	String name;
+	String country;
+	int age;
+	
+	void sports() {
+		System.out.println();
+	}
+}
+
+class TestCase {
+	int Tc_count;
+	String module;
+	String tester;
+	
+	void testing() {
+		System.out.println();
+		
+	}
+}
